e2f9eb92747090f6b7364dcef5878bad1ec3e593
Eric Humphries <ezhumphries@gmail.com>
2020-03-26 13:02:52 -0700

    fixed passport callback to handle existing users

--- services/passport.js
+++ services/passport.js
@@ -16,6 +16,18 @@ passport.use(
       console.log("in index.js accessToken:", accessToken);
       console.log("in index.js accessToken:", refreshToken);
       console.log("in index.js accessToken:", profile);
+
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //user exists already
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+
       const newUser = new User({ googleId: profile.id });
       newUser.save();
     }
