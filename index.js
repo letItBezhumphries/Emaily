8b9fdcab0a8a6a72ebc7eb53a73c1ae3c5e2b9e7
Eric Humphries <ezhumphries@gmail.com>
2020-03-25 23:57:14 -0700

    added options to GoogleStrategy instance

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const passport = require("passport");
+require("dotenv").config();
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL //route user sent to after they grant permissions
+    },
+    accessToken => {
+      console.log("in index.js accessToken:", accessToken);
+    }
+  )
+);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
