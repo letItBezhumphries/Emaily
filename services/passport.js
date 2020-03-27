ec0a6facedee03876c4db1cc0afb2c57b2829aaa
Eric Humphries <ezhumphries@gmail.com>
2020-03-27 16:37:21 -0700

    fixed config require statement

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-require("./config/keys");
+require("../config/keys");
 const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
